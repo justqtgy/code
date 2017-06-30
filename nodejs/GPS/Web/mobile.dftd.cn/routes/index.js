ffd2fc538af33c417d2e49408fc4fca050f3d47d
chenjing <username@email.com>
2017-06-30 15:02:19 +0800

    code

--- nodejs/GPS/Web/mobile.dftd.cn/routes/index.js
+++ nodejs/GPS/Web/mobile.dftd.cn/routes/index.js
@@ -7,33 +7,13 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/index', function(req, res, next) {
-  res.render('index');
+router.get('/boss', function(req, res, next) {
+  res.render('boss/index');
 });
 
-router.get('/m_b_index', function(req, res, next) {
-  var member = req.cookies.member;
-  if(member.boss > 0){
-      res.render('mobile/m_b_index', { title: 'dftd' });
-  }      
-  else{
-      res.redirect('/users/login?url=/m_d_index');
-  }
-      
+router.get('/driver', function(req, res, next) {
+  res.render('driver/index');
 });
-
-router.get('/m_d_index', function(req, res, next) {
-  var member = req.cookies.member;
-  if(member.boss > 0)
-      res.redirect('/users/login?url=/m_b_index');
-  else
-      res.render('mobile/m_d_index', { title: 'dftd' });
-});
-
-router.get('/m_maintenance', function(req, res, next) {
-  res.send('开发中....');
-});
-
 
 router.get('/group', function(req, res, next) {
   res.render('control/_group', { title: 'dftd' });
--- nodejs/GPS/Web/mobile.dftd.cn/routes/params.js
+++
@@ -1,6 +0,0 @@
-exports.params={
-    'alarm_water':4, //含水报警
-    'alarm_impurity':2, //杂质报警
-    'maintenance':8000,//保养
-}
-
